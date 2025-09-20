c469ee0b0e2c60b80348b3e77e462fef37df47b5
Jackie <jackieec956@gmail.com>
2025-09-20 15:11:13 -0400

    :p Resolve merge conflicts in App.tsx and Dashboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { FountainDetail } from "./components/FountainDetail";
 import { FrontPage } from "./components/FrontPage";
 import { UserProfile } from "./components/UserProfile";
 import { FrontPage } from "./components/FrontPage";
+import { UserProfile } from "./components/UserProfile";
 import { supabaseConfig } from "./config/supabase";
 
 const AppContent: React.FC = () => {
